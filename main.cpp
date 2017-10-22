ea8c7cfdf572e38e4ffbd20c1ce46f5e5c0deae2
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-10-22 19:41:19 +0200

    7 tests

--- main.cpp
+++ main.cpp
@@ -7,63 +7,8 @@
 
 int main(int argc, char* argv[]) {
 
-    Dustbin d;
-    d.setColor("green");
-    std::cout<<d.getColor()<<std::endl;
-    std::cout<<d.houseWasteContent[0].getName()<<std::endl;
-    Garbage garbage("garbage1");
-    d.throwOutGarbage(garbage);
-    std::cout<<d.houseWasteContent[0].getName()<<std::endl;
-    Garbage garbage2("garbage2");
-    d.throwOutGarbage(garbage2);
-    std::cout<<d.houseWasteContent[1].getName()<<std::endl;
-    PaperGarbage paper("paper1");
-    try {
-        d.throwOutPaperGarbage(paper);
-    }
-    catch (DustbinContentError &err) {
-        std::cout<<"Exception: "<<err.what()<<std::endl;
-    }
-    paper.squeeze();
-    d.throwOutPaperGarbage(paper);
-    std::cout<<d.paperContent[0].getName()<<std::endl;
-    PaperGarbage paper2("paper2");
-    paper2.squeeze();
-    d.throwOutPaperGarbage(paper2);
-    std::cout<<d.paperContent[1].getName()<<std::endl;
-    PlasticGarbage plastic("plastic1");
-    plastic.clean();
-    try {
-        d.throwOutPlasticGarbage(plastic);
-    } catch (DustbinIsFull &err) {
-        std::cout<<"Exception: "<<err.what()<<std::endl;
-    }
-    d.emptyContents();
-    d.throwOutPlasticGarbage(plastic);
-    std::cout<<d.plasticContent[0].getName()<<std::endl;
-    PlasticGarbage plastic2("plastic2");
-    plastic2.clean();
-    d.throwOutPlasticGarbage(plastic2);
-    std::cout<<d.plasticContent[1].getName()<<std::endl;
-    d.emptyContents();
-    //std::cout<<d.houseWasteContent[0].getName()<<std::endl;
-    //std::cout<<d.paperContent[0].getName()<<std::endl;
-    //std::cout<<d.plasticContent[0].getName()<<std::endl;
-    Garbage garbage3("garbage3");
-    d.throwOutGarbage(garbage3);
-    std::cout<<d.houseWasteContent[0].getName()<<std::endl;
-    PaperGarbage paper3("paper3");
-    paper3.squeeze();
-    d.throwOutPaperGarbage(paper3);
-    std::cout<<d.paperContent[0].getName()<<std::endl;
-    PlasticGarbage plastic3("plastic3");
-    plastic3.clean();
-    d.throwOutPlasticGarbage(plastic3);
-    std::cout<<d.plasticContent[0].getName()<<std::endl;
-
-/*
     testing::InitGoogleTest(&argc, argv);
     RUN_ALL_TESTS();
-*/
+
     return 0;
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -6,6 +6,7 @@
 #include <gmock/gmock.h>
 
 #include "../Dustbin.h"
+#include "../Exceptions.hpp"
 
 using testing::Eq;
 
@@ -19,7 +20,82 @@ namespace {
     };
 }
 
-TEST_F(ClassDeclaration, test1) {
+TEST_F(ClassDeclaration, testDustbinColor) {
     d.setColor("orange");
     ASSERT_EQ("orange", d.getColor());
+}
+
+TEST_F(ClassDeclaration, testThrowOutGarbage) {
+    Garbage garbage("garbage1");
+    d.throwOutGarbage(garbage);
+    ASSERT_EQ("garbage1", d.houseWasteContent[0].getName());
+}
+
+TEST_F(ClassDeclaration, testThrowOutSqueezedPaper) {
+    PaperGarbage paper("paper1");
+    paper.squeeze();
+    d.throwOutPaperGarbage(paper);
+    ASSERT_EQ("paper1", d.paperContent[0].getName());
+}
+
+TEST_F(ClassDeclaration, testFillAfterEmptyContents) {
+    Garbage garbage("garbage1");
+    d.throwOutGarbage(garbage);
+    Garbage garbage2("garbage2");
+    d.throwOutGarbage(garbage2);
+    PaperGarbage paper("paper1");
+    paper.squeeze();
+    d.throwOutPaperGarbage(paper);
+    PaperGarbage paper2("paper2");
+    paper2.squeeze();
+    d.throwOutPaperGarbage(paper2);
+    PlasticGarbage plastic("plastic1");
+    plastic.clean();
+    d.emptyContents();
+    d.throwOutPlasticGarbage(plastic);
+    ASSERT_EQ("plastic1", d.plasticContent[0].getName());
+}
+
+TEST(paperNotSqueezed, what)
+{
+    Dustbin d;
+    PaperGarbage paper("paper1");
+    try {
+        d.throwOutPaperGarbage(paper);
+    } catch (DustbinContentError &err) {
+        EXPECT_STREQ("The alarm is switched on because of DustbinContentError", err.what());
+    }
+}
+
+TEST(plasticNotCleaned, what)
+{
+    Dustbin d;
+    PlasticGarbage plastic("plastic1");
+    try {
+        d.throwOutPlasticGarbage(plastic);
+    } catch (DustbinContentError &err) {
+        EXPECT_STREQ("The alarm is switched on because of DustbinContentError", err.what());
+    }
+}
+
+TEST(dustbinIsFull, what)
+{
+    Dustbin d;
+    Garbage garbage("garbage1");
+    d.throwOutGarbage(garbage);
+    Garbage garbage2("garbage2");
+    d.throwOutGarbage(garbage2);
+    PaperGarbage paper("paper1");
+    paper.squeeze();
+    d.throwOutPaperGarbage(paper);
+    PaperGarbage paper2("paper2");
+    paper2.squeeze();
+    d.throwOutPaperGarbage(paper2);
+    PlasticGarbage plastic("plastic1");
+    plastic.clean();
+    try {
+        d.throwOutPlasticGarbage(plastic);
+    } catch (DustbinIsFull &err) {
+        EXPECT_STREQ("Dustbin is full! You have to empty it before throwing out any more garbage!", err.what());
+    }
 }
